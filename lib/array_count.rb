e708a5f56210fbef50224c15df651df06bb02737
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-15 18:40:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,6 +2,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count_strings {|i| i > 0}
 end
+end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
